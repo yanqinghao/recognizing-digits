c14f2957c8bdfab3b5be5e55c527183b2517b59a
YanQinghao <woshiyanqinghao@gmail.com>
2020-06-22 09:15:12 +0800

    post

--- python/main.py
+++ python/main.py
@@ -9,5 +9,10 @@ def root():
     return app.send_static_file('index.html')
 
 
+@app.route("/data", methods=["POST"])
+def data():
+    pass
+
+
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
