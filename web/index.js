1b3e11c7de31c1788ce53852ac6c061a42a5a1b4
YanQinghao <woshiyanqinghao@gmail.com>
2020-06-23 10:17:56 +0800

    on change function and post

--- web/index.js
+++ web/index.js
@@ -1,28 +1,74 @@
-// import 'https://code.jquery.com/jquery-3.3.1.min.js'
-// import 'https://code.jquery.com/ui/1.12.1/jquery-ui.js'
 import "./node_modules/jquery/dist/jquery.js"
 import "./node_modules/jquery-ui-dist/jquery-ui.js"
-// import "./node_modules/jquery-ui-dist/jquery-ui.css"
-// window.$ = $
-// window.$ = window.jQuery = require('jquery');
 
 $(document).ready(function () {
     var sliders = $(".slider-handle");
     sliders.each(function (index, element) {
         var handle = $(element.getElementsByClassName("ui-slider-handle"));
+        var initValue = (handle.attr("id") == "custom-handle-high-s" || handle.attr("id") == "custom-handle-high-v") ? 255 : 0;
         $(element).slider({
             min: 0,
             max: 255,
             step: 1,
-            value: 0,
+            value: initValue,
             create: function () {
                 handle.text($(this).slider("value"));
             },
             slide: function (event, ui) {
                 handle.text(ui.value);
+            },
+            stop: function (event, ui) {
+                let inputData = getPostData();
+                console.log(inputData);
+                if (inputData.success) {
+                    $.post("/ledDetection", { data: JSON.stringify(inputData.data) }, function (res, status) {
+                        $('#processedimg').attr('src', res.result).css("height", 800);
+                    }, "json");
+                } else {
+                    alert("PLS UPLOAD SOME IMAGE");
+                }
             }
         });
     });
-    $("#slider-high-s").slider("value", 255);
-    $("#slider-high-v").slider("value", 255);
+    $('#uploadfile').change(function () {
+        var input = this;
+        var url = $(this).val();
+        var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+        if (input.files && input.files[0] && (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#showimg').attr('src', e.target.result).css("height", 800);
+                let inputData = getPostData();
+                console.log(inputData)
+                if (inputData.success) {
+                    $.post("/ledDetection", { data: JSON.stringify(inputData.data) }, function (res, status) {
+                        $('#processedimg').attr('src', res.result).css("height", 800);
+                    }, "json");
+                } else {
+                    alert("PLS UPLOAD SOME IMAGE");
+                }
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+        else {
+            alert("PLS UPLOAD SOME IMAGE");
+        }
+    });
 });
+
+function getPostData() {
+    let lowerB = [];
+    let upperB = [];
+    lowerB.push($("#slider-low-h").slider("value"));
+    lowerB.push($("#slider-low-s").slider("value"));
+    lowerB.push($("#slider-low-v").slider("value"));
+    upperB.push($("#slider-high-h").slider("value"));
+    upperB.push($("#slider-high-s").slider("value"));
+    upperB.push($("#slider-high-v").slider("value"));
+    if ($('#showimg').attr("src") == "#") {
+        return { success: false };
+    } else {
+        return { success: true, data: { img: $('#showimg').attr("src").replace(/^data:image\/(png|jpg);base64,/, ""), lowerb: lowerB, upperb: upperB } };
+    }
+
+}
